68d4029ccf37c8c3d94f6ce008c51cdbbb3aaf07
Yaryna Shyika <pinkpoppy.art@gmail.com>
2018-06-10 16:47:16 +0300

    added image resize functionality

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const resizeImage = require('./resizeImage');
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 
 app.use('/resize', resizeImage);
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
+  next();
 });
 
 module.exports = app;
--- server/resizeImage.js
+++ server/resizeImage.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
+const Jimp = require('jimp');
+const sharp = require('sharp');
+var request = require('request');
 
-router.get('/:imageUrl', (req, res) =>{
-  console.log(req.params.imageUrl);
-  res.send('api works! URL = ' + req.params.imageUrl);
+router.get('/', (req, res) =>{
+  var url = req.query.url;
+  request
+    .get(url, (err)=>{
+      if(err) res.status(404).send('Wrong url provided');
+    })
+    .on('response', (response)=>{
+      let mimeType = response.headers['content-type'];
+        res.type(mimeType);
+        resizeImage(response).pipe(res);
+    });
 });
+
+function resizeImage (stream) {
+  let Sharp = sharp();
+  Sharp.resize(640,480);
+  return stream.pipe(Sharp);
+}
 
 module.exports = router;
