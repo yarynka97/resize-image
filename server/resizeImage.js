0a233a0c4e9a2a6adc38675b31662a5a5719a4f4
Yaryna Shyika <yshyika@leobit.co>
2018-06-19 10:59:30 +0300

    started to rewrite code using serverless framework

--- server/resizeImage.js
+++ server/resizeImage.js
@@ -1,14 +1,19 @@
-const router = require('express').Router();
-const sharp = require('sharp');
+/*const router = require('express').Router();
+const sharp = require('sharp');*/
 const request = require('request');
 const imageHash = require('image-hash');
 const Cache = require('streaming-cache');
+const gm = require('gm').subClass({imageMagick: true});;
 
 const cache = new Cache();
 
-router.get('/', (req, res) =>{
-  var url = req.query.url;
+module.exports.resize = function(event, context, callback) {
+  const response = {
+    statusCode: 200
+  };
+  var url = 'https://images.pexels.com/photos/87840/daisy-pollen-flower-nature-87840.jpeg?cs=srgb&dl=bloom-blossom-close-up-87840.jpg';
   imageHash(url, 16, true, (err, data)=>{
+<<<<<<< Updated upstream
     if(err) console.log(err);
     cache.get(data) ?
       cache.get(data).pipe(res):
@@ -19,23 +24,60 @@ router.get('/', (req, res) =>{
         .on('response', (response)=>{
           resize(response, data, res);
         });
+=======
+    var hash = err ? 'else' : data;
+    request.get(url, (err)=>{
+      if(err) {
+        response = {
+          statusCode: 404,
+          body: JSON.stringify({ "message": "Wrong url provided" })
+        };
+      }
+      callback(null, response);
+    })
+    .on('response', (res) => {
+      var img = gm(res)
+      .resize('640', '480')
+      .toBuffer('PNG',function (err, buffer) {
+        buffer.toString('base64');
+        response = {
+          statusCode: 200,
+          isBase64Encoded: true,
+          headers:
+          {
+            “Content-Type”: “image/png”
+          },
+          body: img
+        }
+      });
+      //resize(res, hash);
+    });
+    callback(null, response);
+>>>>>>> Stashed changes
   });
-});
 
-function resize(stream, hash, res){
+};
+
+/*function resize(stream, hash){
   var Sharp = sharp();
-  Sharp.metadata()
+  return Sharp.metadata()
   .then(info=>{
     if(info.width < 640 || info.height < 480){
-      res.status(422).send(`Image is too small to be converted to 640x480; Width: ${info.width} and height: ${info.height}`);
+      return {
+        statusCode: 422,
+        body: JSON.stringify({ "message": `Image is too small to be converted to 640x480; Width: ${info.width} and height: ${info.height}` })
+      };
     } else {
       let mimeType = stream.headers['content-type'];
       res.type(mimeType);
       Sharp.resize(640, 480);
-      stream.pipe(Sharp).pipe(cache.set(hash)).pipe(res);
+      stream.pipe(Sharp).pipe(cache.set(hash));
+      return {
+        statusCode: 200,
+
+      }
     }
   });
   stream.pipe(Sharp);
 }
-
-module.exports = router;
+*/
